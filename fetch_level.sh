51be11248d0333e4da2948f24cceb669eda5a96e
OfekGrunfeld <ofekgrun@gmail.com>
2025-12-12 18:13:18 +0200

    updated utility

--- fetch_level.sh
+++ fetch_level.sh
@@ -2,12 +2,12 @@
 
 # Usage check
 if [ -z "$1" ]; then
-    echo "Usage: $0 <level-name>"
+    echo "Usage: $0 <level-name> [password]"
     exit 1
 fi
 
 LEVEL="$1"
-PASS="guest"
+PASS="${2:-guest}"
 
 # Source
 sshpass -p "$PASS" scp -P 2222 \
@@ -18,3 +18,4 @@ sshpass -p "$PASS" scp -P 2222 \
 sshpass -p "$PASS" scp -P 2222 \
     "${LEVEL}@pwnable.kr:/home/${LEVEL}/${LEVEL}" \
     "./${LEVEL}"
+
